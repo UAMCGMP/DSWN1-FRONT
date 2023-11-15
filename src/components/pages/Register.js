c6ffa1098c9065e27d5bb134740150a8efc8851f
Camila Caetano <camilaanjos478@gmail.com>
2023-11-15 12:08:19 -0300

    feat.: add fluxo de registro

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import { Redirect } from 'react-router-dom'
 
 function Register() {
+    const [redirect, setRedirect] = useState(false);
+
+    async function checkCredentials(){
+        const inputEmail = document.querySelector(".input-email").value
+        const inputPassword = document.querySelector(".input-password").value
+
+        if (!inputEmail || !inputPassword){
+            alert("Favor preencher os campos")
+            return
+        }
+
+        try {
+            await doRegister(inputEmail, inputPassword);
+        } catch (error) {
+            console.log(error);
+        }
+        
+
+    }
+
+    async function doRegister(login, senha){
+        axios.post('http://localhost:8080/auth/register/user', {
+            "login": login,
+            "password": senha
+          })
+          .then(function (response) {
+            alert ("Cadastro concluido com sucesso, você será redirecionado para o login!")
+            setRedirect(true);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
     return (
         <div className='container'>
+             {redirect && <Redirect to="/login"/>}
 
             <h3 className='titulo-login'>Para registrar-se, insira os dados nos campos abaixo:</h3>
 
@@ -15,11 +51,7 @@ function Register() {
                 className='input-password'
                 placeholder='Senha' />
 
-            <input type='password'
-                className='input-password'
-                placeholder='Repita a senha' />
-
-            <a href="/" className="botao-login">Registrar</a>
+            <a onClick={checkCredentials} className="botao-login">Registrar</a>
 
         </div>
 
